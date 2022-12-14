1601ab2bd5d99071f29b1083e240f615a261336f
Fimost <traorenabiladama460@gmail.com>
2022-12-14 16:43:58 +0100

    ajouter

---
+++ admin/database.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+//connection base de donnée
+
+$pdo = new  PDO('mysql:host=localhost;dbname=magazone','root','root',array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+));
+
+?>
+
+
+
